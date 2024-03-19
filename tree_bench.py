5480ce819c28b1bb6611cfdbfe2fe16672194d35
Ashton Snelgrove <ashton@snelgrove.science>
2024-03-19 11:08:14 -0600

    binary tree verilog generator.

---
+++ tree_bench.py
@@ -0,0 +1,23 @@
+import sys
+width, depth = [int(i) for i in sys.argv[1:]]
+
+
+def tree(output, depth):
+    left = f"{output}__{depth}_l"
+    right = f"{output}__{depth}_r"
+    print(f"diffmix_25px_0 mix_{output} (.a_fluid({left}), .b_fluid({right}), .out_fluid({output});")
+    print(f"wire {left}, {right};")
+    if depth == 0:
+        return [left, right]
+    else:
+        return tree(left, depth - 1) + tree(right, depth - 1)
+
+print(f"""module binary_tree_{width}_{depth} (""")
+print(",".join([f"output out_{i}" for i in range(0,width)] + [f"input_{j}" for j in range(0,width*2**depth)]))
+print(");")
+
+inpts = [t for i in range(0, width) for t in tree(f"input_{i}", depth-1)]
+assert(len(inpts) == width*2**depth)
+for i, inpt in enumerate(inpts):
+    print(f"assign input_{i} = {inpt};")
+print("endmodule")
