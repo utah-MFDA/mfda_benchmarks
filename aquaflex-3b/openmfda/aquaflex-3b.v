4aca1913db2f694a2fe129c6cf24d205e0522bbf
Ashton Snelgrove <ashton@snelgrove.science>
2024-05-01 11:50:27 -0600

    Cleaned versions of aquaflex

--- aquaflex-3b/openmfda/aquaflex-3b.v
+++ aquaflex-3b/openmfda/aquaflex-3b.v
@@ -1,4 +1,4 @@
-module AquaFlex-3b(
+module aquaflex_3b(
 input InputB,
 input InputC,
 input InputD,
@@ -8,31 +8,31 @@ output OutH,
 output OutI,
 output OutJ
 );
-wire flow_switch4_0-InputC;
-wire flow_switch4_0-InputD;
-wire flow_switch4_0-InputE;
-wire flow_switch4_0-flow_switch4_1;
-wire flow_switch4_1-InputB;
-wire flow_switch4_1-InputF;
-wire flow_switch4_1-PumpA;
-wire PumpA-Mixer1;
-wire Mixer1-PumpC;
-wire PumpC-flow_switch4_2;
-wire flow_switch4_2-OutH;
-wire flow_switch4_2-OutI;
-wire flow_switch4_2-OutJ;
-assign flow_switch4_1-InputB = InputB;
-assign flow_switch4_0-InputC = InputC;
-assign flow_switch4_0-InputD = InputD;
-assign flow_switch4_0-InputE = InputE;
-assign flow_switch4_1-InputF = InputF;
-assign OutH = flow_switch4_2-OutH;
-assign OutI = flow_switch4_2-OutI;
-assign OutJ = flow_switch4_2-OutJ;
-Mixer Mixer1(.port0(PumpA-Mixer1),.port1(Mixer1-PumpC));
-Filter PumpA(.port0(PumpA-Mixer1),.port1(flow_switch4_1-PumpA));
-Filter PumpC(.port0(Mixer1-PumpC),.port1(PumpC-flow_switch4_2));
-Switch flow_switch4_0(.port0(flow_switch4_0-InputD),.port1(flow_switch4_0-flow_switch4_1),.port2(flow_switch4_0-InputC),.port3(flow_switch4_0-InputE));
-Switch flow_switch4_1(.port0(flow_switch4_1-InputF),.port1(flow_switch4_0-flow_switch4_1),.port2(flow_switch4_1-PumpA),.port3(flow_switch4_1-InputB));
-Switch flow_switch4_2(.port0(PumpC-flow_switch4_2),.port1(flow_switch4_2-OutJ),.port2(flow_switch4_2-OutH),.port3(flow_switch4_2-OutI));
+wire flow_switch4_0_InputC;
+wire flow_switch4_0_InputD;
+wire flow_switch4_0_InputE;
+wire flow_switch4_0_flow_switch4_1;
+wire flow_switch4_1_InputB;
+wire flow_switch4_1_InputF;
+wire flow_switch4_1_PumpA;
+wire PumpA_Mixer1;
+wire Mixer1_PumpC;
+wire PumpC_flow_switch4_2;
+wire flow_switch4_2_OutH;
+wire flow_switch4_2_OutI;
+wire flow_switch4_2_OutJ;
+assign flow_switch4_1_InputB = InputB;
+assign flow_switch4_0_InputC = InputC;
+assign flow_switch4_0_InputD = InputD;
+assign flow_switch4_0_InputE = InputE;
+assign flow_switch4_1_InputF = InputF;
+assign OutH = flow_switch4_2_OutH;
+assign OutI = flow_switch4_2_OutI;
+assign OutJ = flow_switch4_2_OutJ;
+mixer Mixer1(.port0(PumpA_Mixer1),.port1(Mixer1_PumpC));
+filter PumpA(.port0(PumpA_Mixer1),.port1(flow_switch4_1_PumpA));
+filter PumpC(.port0(Mixer1_PumpC),.port1(PumpC_flow_switch4_2));
+junction4 flow_switch4_0(.port0(flow_switch4_0_InputD),.port1(flow_switch4_0_flow_switch4_1),.port2(flow_switch4_0_InputC),.port3(flow_switch4_0_InputE));
+junction4 flow_switch4_1(.port0(flow_switch4_1_InputF),.port1(flow_switch4_0_flow_switch4_1),.port2(flow_switch4_1_PumpA),.port3(flow_switch4_1_InputB));
+junction4 flow_switch4_2(.port0(PumpC_flow_switch4_2),.port1(flow_switch4_2_OutJ),.port2(flow_switch4_2_OutH),.port3(flow_switch4_2_OutI));
 endmodule
