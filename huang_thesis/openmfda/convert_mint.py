a0df8481cc0b4d032f123b64fec4c7da21dd1105
Ashton Snelgrove <ashton@snelgrove.science>
2024-05-02 13:14:19 -0600

    Rough mint conversion script ready

--- huang_thesis/openmfda/convert_mint.py
+++ huang_thesis/openmfda/convert_mint.py
@@ -7,15 +7,25 @@ print(f"module {dev.name} (")
 print("inout", ", ".join([component.name for component in dev.components if component.entity == "PORT"]))
 print(");")
 
+valve_nets = [dev.get_valve_connection(v).name for v in dev.valves]
+
 for connection in dev.connections:
+    if connection.name in valve_nets:
+        print(f"wire {connection.name}_out")
     print(f"wire {connection.name};")
 
 for component in dev.components:
+    connections = dev.get_connections_for_component(component)
     if component.entity == "PORT":
         continue
-    connections = dev.get_connections_for_component(component)
-    ports = [f".port{c.source.port or 0}({c.name})" for c in connections if c.source.component == component.ID] + \
-        [f".port{s.port or 0}({c.name})" for c in connections for s in c.sinks if s.component == component.ID]
+
+    ports = [f".port{c.source.port or 0}({c.name})" for c in connections if c.source.component == component.ID]
+    ports += [f".port{s.port or 0}({c.name})" for c in connections for s in c.sinks if s.component == component.ID and c.name not in valve_nets]
+    ports += [f".port{s.port or 0}({c.name}_out)" for c in connections for s in c.sinks if s.component == component.ID and c.name in valve_nets]
+
+    if component.entity == "VALVE":
+        ports += [".out(" + dev.get_valve_connection(component).name + "_out)",
+                  ".in(" + dev.get_valve_connection(component).name + ")"]
     ports = ",".join(ports)
     print(f"{component.entity} {component.name}({ports});")
 print("endmodule")
