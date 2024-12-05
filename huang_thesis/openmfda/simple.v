1a3b4133b2178ada07650664f9a056d995b316be
Ashton Snelgrove <ashton@snelgrove.science>
2024-12-05 16:53:03 -0700

    Fix input/output directions for huang/simple

--- huang_thesis/openmfda/simple.v
+++ huang_thesis/openmfda/simple.v
@@ -1,12 +1,12 @@
 module simple (
-inout p1, p2, p3, cp1, cp2
+output p1, p2, input p3, input cp1, cp2
 );
   wire c3;
-LONG_CELL_TRAP ct1(.port2(p3),.port1(c3));
-valve v1(.air_in(cp1),.fluid_out(c3),.fluid_in(p1));
-valve v2(.air_in(cp2),.fluid_out(c3),.fluid_in(p2));
+LONG_CELL_TRAP ct1(.port1(p3),.port2(c3));
+valve v1(.air_in(cp1),.fluid_in(c3),.fluid_out(p1));
+valve v2(.air_in(cp2),.fluid_in(c3),.fluid_out(p2));
 endmodule
 
-module LONG_CELL_TRAP(inout port2, port1);
-  serpentine c(port2, port1);
+module LONG_CELL_TRAP(input port1, output port2);
+  serpentine_50px_0 c(.in_fluid(port1), .out_fluid(port2));
 endmodule
